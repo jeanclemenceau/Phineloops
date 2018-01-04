544725ab05221bc1a644bc149a78e65da1097434
Jean Clemenceau <jean.clemenceau@dauphine.eu>
2018-01-04 16:07:20 +0100

    now using an enum to store constants data about pieces

--- src/main/java/fr/dauphine/javaavance/phineloops/model/Piece.java
+++ src/main/java/fr/dauphine/javaavance/phineloops/model/Piece.java
@@ -1,10 +1,5 @@
 package fr.dauphine.javaavance.phineloops.model;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Piece {
 	private int num;
 	private int orientation;
@@ -13,52 +8,20 @@ public class Piece {
 	private boolean fixed = false;
 	private int x, y;
 	
-	public static Map<Integer, Integer[]> correspondence = new HashMap<>(); // give orientationMax and nbConnections for a pieceNumber
-	
-	static {
-		correspondence.put(0, new Integer[] {0,0}); 
-		correspondence.put(1, new Integer[] {3,1}); // max orientation 3 max connections 1
-		correspondence.put(2, new Integer[] {1,2});
-		correspondence.put(3, new Integer[] {3,3});
-		correspondence.put(4, new Integer[] {0,4});
-		correspondence.put(5, new Integer[] {3,2});
-	}
-	
-	public static Map<List<Integer>, String> unicodeMatch = new HashMap<>();
-	
-	static {
-		unicodeMatch.put(Arrays.asList(new Integer[] {0,0}), "\u2205");
-		unicodeMatch.put(Arrays.asList(new Integer[] {1,0}), "\u2579");
-		unicodeMatch.put(Arrays.asList(new Integer[] {1,1}), "\u257A");
-		unicodeMatch.put(Arrays.asList(new Integer[] {1,2}), "\u257B");
-		unicodeMatch.put(Arrays.asList(new Integer[] {1,3}), "\u2578");
-		unicodeMatch.put(Arrays.asList(new Integer[] {2,0}), "\u2503");
-		unicodeMatch.put(Arrays.asList(new Integer[] {2,1}), "\u2501");
-		unicodeMatch.put(Arrays.asList(new Integer[] {3,0}), "\u253B");
-		unicodeMatch.put(Arrays.asList(new Integer[] {3,1}), "\u2523");
-		unicodeMatch.put(Arrays.asList(new Integer[] {3,2}), "\u2533");
-		unicodeMatch.put(Arrays.asList(new Integer[] {3,3}), "\u252B");
-		unicodeMatch.put(Arrays.asList(new Integer[] {4,0}), "\u254B");
-		unicodeMatch.put(Arrays.asList(new Integer[] {5,0}), "\u2517");
-		unicodeMatch.put(Arrays.asList(new Integer[] {5,1}), "\u250F");
-		unicodeMatch.put(Arrays.asList(new Integer[] {5,2}), "\u2513");
-		unicodeMatch.put(Arrays.asList(new Integer[] {5,3}), "\u251B");
-	}
-	
 	public Piece(int num, int orientation) {
 		this.num = num;
 		this.orientation = orientation;
-		orientationMax = correspondence.get(num)[0];
-		nbConnections = correspondence.get(num)[1];
+		orientationMax = PieceProperties.getIdentifier(num).getOrientationMax();
+		nbConnections = PieceProperties.getIdentifier(num).getNbConnections();
 	}
-	
+
 	public Piece(int num, int orientation, int x, int y) {
 		this.num = num;
 		this.orientation = orientation;
-		orientationMax = correspondence.get(num)[0];
-		nbConnections = correspondence.get(num)[1];
 		this.x = x;
 		this.y = y;
+		orientationMax = PieceProperties.getIdentifier(num).getOrientationMax();
+		nbConnections = PieceProperties.getIdentifier(num).getNbConnections();
 	}
 	
 	/***
@@ -96,7 +59,7 @@ public class Piece {
 	}
 
 	public String toUnicode() {
-		return unicodeMatch.get(Arrays.asList(new Integer[] {num, orientation}));
+		return PieceProperties.getUnicode(num, orientation);
 	}
 
 	@Override
---
+++ src/main/java/fr/dauphine/javaavance/phineloops/model/PieceProperties.java
@@ -0,0 +1,109 @@
+package fr.dauphine.javaavance.phineloops.model;
+
+/***
+ * Stores constant properties of pieces and allow retrieving some of them in a static context (unicode, cardinal points of connections) with keys arguments
+ * Identifiers are the numbers of the pieces preceded by a bottom dash.
+ */
+public enum PieceProperties {
+	_0(0,0),
+	_1(3,1),
+	_2(1,2),
+	_3(3,3),
+	_4(0,4),
+	_5(3,2);
+	
+	private final int orientationMax;
+	private final int nbConnections;
+	
+	PieceProperties(int orientationMax, int nbConnections) {
+		this.orientationMax = orientationMax;
+		this.nbConnections = nbConnections;
+	}
+	
+	public int getOrientationMax() {
+		return orientationMax;
+	}
+
+	public int getNbConnections() {
+		return nbConnections;
+	}
+	
+	public static String getUnicode(int num, int orientation) {
+		switch (num) {
+		case 0:
+			return "\u2205";
+		case 1:
+			if(orientation == 0) return "\u2579";
+			if(orientation == 1) return "\u257A";
+			if(orientation == 2) return "\u257B";
+			if(orientation == 3) return "\u2578";
+		case 2:
+			if(orientation == 0) return "\u2503";
+			if(orientation == 1) return "\u2501";
+		case 3:
+			if(orientation == 0) return "\u253B";
+			if(orientation == 1) return "\u2523";
+			if(orientation == 2) return "\u2533";
+			if(orientation == 3) return "\u252B";
+		case 4:
+			return "\u254B";
+		case 5:
+			if(orientation == 0) return "\u2517";
+			if(orientation == 1) return "\u250F";
+			if(orientation == 2) return "\u2513";
+			if(orientation == 3) return "\u251B";
+		default:
+			return "\u000F";
+		}
+	}
+	
+	/***
+	 * Fetch connections available for each cardinal point for a piece depending of its orientation
+	 * @param num
+	 * @param orientation
+	 * @return an array of integers which contains number of connections for a cardinal point. 
+	 * Index 0 is for the North, index 1 is for the East, index 2 is for the South, index 3 is for the West
+	 */
+	public static int[] getLinksOnCardinalPoints(int num, int orientation) {
+		int[] links = new int[4];
+		switch(num) {
+		case 0: 
+			break;
+		case 1:
+			if(orientation == 0) links[0] = 1;
+			if(orientation == 1) links[1] = 1;
+			if(orientation == 2) links[2] = 1;
+			if(orientation == 3) links[3] = 1;
+		case 2:
+			if(orientation == 0) {links[0] = 1; links[2] = 1;}
+			if(orientation == 1) {links[1] = 1; links[3] = 1;}
+		case 3:
+			if(orientation == 0) {links[0] = 1; links[1] = 1; links[3] = 1;}
+			if(orientation == 1) {links[0] = 1; links[1] = 1; links[2] = 1;}
+			if(orientation == 2) {links[1] = 1; links[2] = 1; links[3] = 1;}
+			if(orientation == 3) {links[0] = 1; links[2] = 1; links[3] = 1;}
+		case 4:
+			{links[1] = 1; links[2] = 1; links[3] = 1; links[4] = 1;}
+		case 5:
+			if(orientation == 0) {links[0] = 1; links[1] = 1;}
+			if(orientation == 1) {links[1] = 1; links[2] = 1;}
+			if(orientation == 2) {links[2] = 1; links[3] = 1;}
+			if(orientation == 3) {links[3] = 1; links[0] = 1;}
+		default:
+		}
+		return links;
+	}
+
+	public static PieceProperties getIdentifier(int num) {
+		switch(num) {
+		case 0: return _0;
+		case 1: return _1;
+		case 2: return _2;
+		case 3: return _3;
+		case 4: return _4;
+		case 5: return _5;
+		default: return null;
+		}
+	}
+	
+}
