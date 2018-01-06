ea34b2f185c6a56b9f5c19ca7f008154a4488cf4
Bismuth <betty.bismuth12@gmail.com>
2018-01-06 16:41:46 +0100

    add methods to get neighbours

--- src/main/java/fr/dauphine/javaavance/phineloops/model/Grid.java
+++ src/main/java/fr/dauphine/javaavance/phineloops/model/Grid.java
@@ -8,11 +8,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 
 public class Grid {
 	private int width;
 	private int height;
-	private List<Piece> pieces = new ArrayList<>();
+	private Piece[][] pieces;
 	
 	public Grid(String fileName) throws Exception {
 		File f = new File(fileName);
@@ -23,13 +24,14 @@ public class Grid {
 			
 			width = Integer.parseInt(reader.readLine());
 			height = Integer.parseInt(reader.readLine());	
+			pieces = new Piece[width][height];
 			
 			for(int i = 0; i<height; i++) {
 				for(int j = 0; j<width; j++) {
 					line = reader.readLine();
 					if(!line.equals(null)) {
 						data = line.split(" ");
-						pieces.add(new Piece(Integer.parseInt(data[0]), Integer.parseInt(data[1]), j, i));
+						pieces[j][i] = new Piece(Integer.parseInt(data[0]), Integer.parseInt(data[1]));
 					}
 					else {
 						reader.close();
@@ -53,10 +55,12 @@ public class Grid {
 			FileWriter writer = new FileWriter(f);
 			String ls = System.lineSeparator();
 			StringBuilder builder = new StringBuilder(width + ls + height);
-			
-			for (Piece piece : pieces) 
-				builder.append(ls + piece.getNum() + " " + piece.getOrientation());
-			
+			for(int i= 0; i< height; i++) {
+				for(int j=0; j<width; j++) {
+					builder.append(ls + pieces[j][i].getNum() + " " + pieces[j][i].getOrientation());
+				}
+			}
+					
 			writer.write(builder.toString());
 			writer.close();
 		} catch (IOException e) {
@@ -64,15 +68,65 @@ public class Grid {
 		}	
 	}
 	
-	public void print() {
-		for (Piece piece : pieces) {
-			System.out.print(piece.toUnicode());
-			if(piece.getX() == width-1) System.out.println();
+	//neighbours[]: 0 north, 1 east, 2 south, 3 west
+	public Piece[] getPieceNeighbours(Piece p) {
+		int x = p.getX();
+		int y = p.getY();
+		Piece[] neighbours = new Piece[4];
+		
+		if(y==0)
+			neighbours[0] = null;
+		else 
+			neighbours[0] = pieces[x][y-1];
+		
+		if(x==width-1) 
+			neighbours[1]=null;
+		else 
+			neighbours[1]=pieces[x+1][y];		
+		
+		if(y==height-1)
+			neighbours[2]= null;
+		else 
+			neighbours[2] = pieces[x][y+1];
+		
+		if(x==0) 
+			neighbours[3] = null;
+		else 
+			neighbours[3] = pieces[x-1][y];
+	
+		return neighbours;
+	}
+	
+	public HashMap<Piece, Piece[]> getAllNeighbours(){
+		HashMap<Piece,Piece[]> allNeighbours = new HashMap<Piece, Piece[]>();
+		for(int i=0; i<height; i++) {
+			for(int j=0; j<width; j++) {
+				allNeighbours.put(pieces[j][i], getPieceNeighbours(pieces[j][i]));
+			}
 		}
+		return allNeighbours;
+	}
+	
+	public void print() {
+		for(int i= 0; i< height; i++) {
+			for(int j=0; j<width; j++) {
+				System.out.print(pieces[j][i].toUnicode());
+				if(pieces[j][i].getX() == width-1) System.out.println();
+			}
+		}
+		
 	}
 
-	public List<Piece> getPieces() {
+	public Piece[][] getPieces() {
 		return pieces;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
 	}
 	
 }
