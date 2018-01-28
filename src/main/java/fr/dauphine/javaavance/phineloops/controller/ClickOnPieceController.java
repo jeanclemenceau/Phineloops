8300dccabf227fd5f0cae509e6c21146535904e6
Jean Clemenceau <jean.clemenceau@dauphine.eu>
2018-01-28 12:49:44 +0100

    updated docs

--- src/main/java/fr/dauphine/javaavance/phineloops/controller/ClickOnPieceController.java
+++ src/main/java/fr/dauphine/javaavance/phineloops/controller/ClickOnPieceController.java
@@ -5,14 +5,32 @@ import java.awt.event.ActionListener;
 
 import fr.dauphine.javaavance.phineloops.model.Piece;
 
+/**
+ * Handle an user clic on a PieceDisplay
+ *@see ActionListener
+ */
 public class ClickOnPieceController implements ActionListener {
 	
+	/**
+	 * The target piece
+	 * @see Piece
+	 */
 	private final Piece p;
 
+	/**
+	 * Constructor
+	 * <p>
+	 * Initialize p property
+	 * </p>
+	 * @param p the piece to reference
+	 */
 	public ClickOnPieceController(Piece p) {
 		this.p = p;
 	}
 	
+	/**
+	 * A click on the associated PieceDisplay triggers a 90° clockwise rotation of the referenced piece
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		p.pivot();	
